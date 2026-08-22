cad86cec3e36dad9353bf85c5bfb485285b0c224
Jati Agung <jatiagung.idn@gmail.com>
2026-08-22 14:30:42 +0000

    chore: setup express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
-console.log("Ledgerly server");
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
